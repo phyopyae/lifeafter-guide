755dfd01cc569ff876622f638e0d71d35aac095a
Phyo Pyae <phyopyae.acm@gmail.com>
2020-06-29 18:19:43 +0800

    Add reference video page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,23 @@ export class AppComponent {
   title = 'lifeafter-food-recipes';
   food_recipes: boolean = true;
   certificates: boolean = false;
+  videoguide: boolean = false;
 
   onGoToFoodRecipes() {
     this.food_recipes = true;
     this.certificates = false;
+    this.videoguide = false;
   }
 
   onGoToCertificates() {
     this.food_recipes = false;
     this.certificates = true;
+    this.videoguide = false;
+  }
+
+  onGoToVideoGuide() {
+    this.food_recipes = false;
+    this.certificates = false;
+    this.videoguide = true;
   }
 }
