accf6a12a6bce8d9c8b60efd3d6f437ac6fe168b
Phyo Pyae <phyopyae.acm@gmail.com>
2020-06-30 17:02:04 +0800

    Add routing for each tab of food, certificate and video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { VideoComponent } from './video/video.component';
+import { CertificateComponent } from './certificate/certificate.component';
+import { FoodComponent } from './food/food.component';
 
-
+const routes: Routes = [
+  { path: 'video', component: VideoComponent },
+  { path: 'certificate', component: CertificateComponent },
+  { path: 'food', component: FoodComponent },
+  { path: '', redirectTo: '/food', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,RouterModule.forRoot(routes),HttpClientModule
+  ],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { VideoComponent } from './video/video.component';
+import { CertificateComponent } from './certificate/certificate.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoComponent,
+    CertificateComponent,
+    FoodComponent
   ],
   imports: [
     BrowserModule,
