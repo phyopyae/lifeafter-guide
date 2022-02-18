b5686cebd0665ba973d254142c26c446d9e1db37
phyopyae <phyopyae2@gmail.com>
2022-02-18 14:58:48 +0630

    change project name and add node engine

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/lifeafter-food-recipes'));
+app.use(express.static('./dist/lifeafter-guide'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/lifeafter-food-recipes/'}),
+    res.sendFile('index.html', {root: 'dist/lifeafter-guide/'}),
 );
 
 app.listen(process.env.PORT || 8080);
